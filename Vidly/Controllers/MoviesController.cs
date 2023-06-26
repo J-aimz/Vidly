946f77a9d81786de584e30a1dd6cbf595c6e6912
Finejojo <108051271+Finejojo@users.noreply.github.com>
2023-06-26 23:51:07 +0100

    first commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            Movie movie = new Movie()
+            {
+                Name = "Avatar"
+            };
+
+            return View(movie);
+        }
+
+        public IActionResult Edit(int  movieId)
+        {
+            return Content("id : " +  movieId);
+        }
+    }
+}
